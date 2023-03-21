e37aa183a7afbf4ee4e27077b2ee24b289f97195
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-21 19:12:33 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp79
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Бам бум бим бам";
+            string[] bam = text.Split(' ');
+
+            foreach (string bamLine in bam)
+            {
+                Console.WriteLine(bamLine);
+            }
+        }
+    }
+}
